a505db70c43852f44a50d8a840ce4c0fc715f44a
Rnksh <rnksh707@gmail.com>
2023-11-05 21:34:41 +0300

    Create Validator.php

---
+++ Validator.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace Imy\Core;
+abstract class Validator
+{
+
+    protected $data = [];
+
+    protected $request;
+
+    protected $fields = [];
+
+    private $errors = [];
+
+    public function __construct($data = [], $files = [])
+    {
+        $this->request = new Request();
+
+        $this->data = $data ?: $this->request->post();
+        $this->dataFiles = $files ?: $this->request->files();
+
+        $this->init();
+    }
+
+    abstract function init();
+
+    public function check()
+    {
+
+        foreach ($this->fields as $field => $rules) {
+            $value = $this->data[$field] ?? false;
+            $fileValue = $this->files[$field] ?? [];
+
+            foreach ($rules as $rule => $error) {
+                $rule = explode(':', $rule);
+                $message = gettype($error) == 'boolean' ? $rules['message'] : $error;
+
+                $error = false;
+
+                switch ($rule[0]) {
+                    case 'required':
+                        if (empty($value))
+                            $error = true;
+                        break;
+
+                    case 'type':
+                        if (!Data::check($value, $rule[1]) && !empty($value))
+                            $error = true;
+                        break;
+
+                    case 'length':
+                        if (strlen($value) < $rule[1] && !empty($value))
+                            $error = true;
+                        break;
+
+                    case 'regexp':
+                        if (!preg_match($rule[1], $value) && !empty($value))
+                            $error = true;
+                        break;
+
+                    case 'size':
+                        if ($fileValue['size'] > $rule[1])
+                            $error = true;
+                        break;
+                }
+
+                if ($error) {
+                    $this->errors[$field][] = $message;
+                    $isCritical = array_pop($rule) == 'critical';
+
+                    if ($isCritical)
+                        return false;
+                }
+
+            }
+
+            if (method_exists($this, $field) && !empty($value)) {
+                $test = $this->{$field}($value);
+
+                if ($test !== true) {
+                    $message = explode('critical:', $test);
+                    if (count($message) > 1) {
+                        $this->errors[$field][] = $message[1];
+                        return false;
+                    } else {
+                        $this->errors[$field][] = $message[0];
+                    }
+                }
+            }
+
+        }
+
+        return empty($this->errors);
+    }
+
+    public function getErrors($array = false)
+    {
+
+        if ($array)
+            return $this->errors;
+
+        $separator = "\n";
+
+        $result = [];
+        foreach ($this->errors as $error) {
+            $result[] = implode($separator, $error);
+        }
+
+        return implode($separator, $result);
+    }
+}
