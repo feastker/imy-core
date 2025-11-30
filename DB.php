587125222c7fa9fa76d2b8acf5c9a1929af6fec6
Rnksh <rnksh707@gmail.com>
2025-12-01 00:16:47 +0300

    Update DB.php

--- DB.php
+++ DB.php
@@ -164,35 +164,49 @@ class DB
 //                Profiler::log($query, 'mysql');
 //            }
         } catch (\PDOException $e) {
+
             if (strpos($e->getMessage(), 'unbuffered queries') !== false ||
                 strpos($e->getMessage(), '2014') !== false ||
                 strpos($e->getMessage(), 'Cannot execute queries') !== false) {
 
                 $errorMsg = "\n=== ERROR: Unbuffered query detected ===\n";
                 $errorMsg .= "Error: " . $e->getMessage() . "\n";
-                $errorMsg .= "Problematic query: " . substr($query, 0, 500) . "\n\n";
+                $errorMsg .= "Problematic query: " . substr($query, 0, 500) . "\n";
+                $errorMsg .= "Connection: " . $this->connection_name . "\n";
+                $errorMsg .= "PDO Persistent: " . ($this->pdo->getAttribute(\PDO::ATTR_PERSISTENT) ? 'YES (PROBLEM!)' : 'NO') . "\n";
+                $errorMsg .= "Buffered Queries: " . ($this->pdo->getAttribute(\PDO::MYSQL_ATTR_USE_BUFFERED_QUERY) ? 'YES' : 'NO') . "\n\n";
 
                 if (class_exists('Imy\Core\Debug')) {
                     $allQueries = Debug::getQueries();
                     $errorMsg .= "All queries executed before this error (" . count($allQueries) . " total):\n";
                     $errorMsg .= str_repeat("=", 80) . "\n";
 
-                    foreach ($allQueries as $index => $q) {
-                        $errorMsg .= sprintf(
-                            "[%d] [%.3fms] [%s] %s\n",
-                            $index + 1,
-                            $q['time'] * 1000,
-                            $q['connection'] ?? 'default',
-                            substr($q['sql'], 0, 200)
-                        );
+                    if (count($allQueries) > 0) {
+                        foreach ($allQueries as $index => $q) {
+                            $errorMsg .= sprintf(
+                                "[%d] [%.3fms] [%s] %s\n",
+                                $index + 1,
+                                $q['time'] * 1000,
+                                $q['connection'] ?? 'default',
+                                substr($q['sql'], 0, 200)
+                            );
+                        }
+                    } else {
+                        $errorMsg .= "NOTE: Debug class shows 0 queries. This may indicate:\n";
+                        $errorMsg .= "  - Debug was not initialized before first query\n";
+                        $errorMsg .= "  - Queries were executed before Debug::init()\n";
+                        $errorMsg .= "  - Persistent connection from previous request has unclosed cursor\n";
                     }
                     $errorMsg .= str_repeat("=", 80) . "\n";
+                } else {
+                    $errorMsg .= "NOTE: Debug class is not available. Cannot show query history.\n";
+                    $errorMsg .= "This may indicate queries were executed before Debug::init()\n";
                 }
 
 
-                $backtrace = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, 10);
+                $backtrace = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, 15);
                 $errorMsg .= "\nCall stack:\n";
-                foreach (array_slice($backtrace, 0, 8) as $i => $frame) {
+                foreach (array_slice($backtrace, 0, 12) as $i => $frame) {
                     if (isset($frame['file']) && isset($frame['line'])) {
                         $file = str_replace(getCWD(), '', $frame['file']);
                         $errorMsg .= sprintf("  [%d] %s:%d %s()\n",
